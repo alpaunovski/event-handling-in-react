1b5f3dc25f8f2a432086432b345fe4e9ec234920
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-01-31 11:51:08 +0200

    finished app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
 function App() {
+  const [style, setStyle] = React.useState({
+    //Initialize the state with a style object
+  });
+
+  function handleMouseOver(){
+    return setStyle({
+      backgroundColor: "black",
+      color: "white",
+    });
+
+  }
+
+  function handleMouseOut(){
+    return setStyle({
+    //Clear the style we set in the mouseover function
+    });
+  }
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} style={style}>Submit</button>
     </div>
   );
 }
